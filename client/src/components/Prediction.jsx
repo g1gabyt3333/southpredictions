e6e25f0e002068105ebaea6017e65b3aac9ec765
AKarp123 <87674424+AKarp123@users.noreply.github.com>
2022-08-24 17:12:12 -0400

    can now vote

--- client/src/components/Prediction.jsx
+++ client/src/components/Prediction.jsx
@@ -1,6 +1,6 @@
 import * as React from "react";
 import * as app from "../firebase";
-import { useCollectionData } from "react-firebase-hooks/firestore";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
@@ -10,14 +10,16 @@ import { Chip, requirePropFactory } from "@mui/material";
 import Results from "./Results";
 import { Divider } from "@mui/material";
 
-export default function Prediction({ data, user }) {
-    
+export default function Prediction({ data }) {
     const padding = { paddingLeft: "5px", paddingRight: "5px" };
 
-    // const [user, loading, error] = useAuthState(app.auth);
-    // const query = app.db.doc(`/predictions/${data.id}/votes/${user.uid}`);
-    // const [value, load, e] = useCollectionData(query);
-    
+    const [user] = useAuthState(app.auth);
+    const query = app.db
+        .collection("/predictions")
+        .doc(data.id)
+        .collection("/votes")
+        .doc(user.uid);
+    const [value] = useDocumentData(query, { idField: "id" });
 
     const addVote = (e) => {
         const ref = app.db
@@ -33,43 +35,53 @@ export default function Prediction({ data, user }) {
 
     const handleClick = (e) => {
         console.log(e.target.innerText);
-        addVote(e)
+        addVote(e);
     };
+    // data.answer === option ? \
 
-    return (
-        <Card sx={{ minWidth: 275 }}>
-            <CardContent>
-                <Typography
-                    sx={{ fontSize: 14 }}
-                    color="text.secondary"
-                    gutterBottom
-                >
-                    {data.dateCreated !== null
-                        ? new Date(data.dateCreated.seconds * 1000).toString()
-                        : "..."}
-                </Typography>
-                <Typography variant="h5" component="div">
-                    {data.prediction}
-                    {data.isCompleted ? " (Completed)" : " (Pending)"}
-                </Typography>
-            </CardContent>
-            <CardActions>
-                {data.options.map((option, index) => (
-                    <Chip
-                        key={index}
-                        variant={data.isCompleted ? "outlined" : "filled"}
-                        label={option}
-                        onClick={data.isCompleted ? null : handleClick}
-                        color={data.answer === option ? "primary" : "default"}
-                        sx={{ ...padding }}
-                    />
-                ))}
-            </CardActions>
-            <Divider />
-            <Results
-                results={data.results}
-                answer={data.answer}
-            />
-        </Card>
-    );
-}
+    const chipColor = (option) => {
+        if (option === data.answer) {
+            return "success";
+        } else if (value && value.vote === option) {
+            return "primary";
+        } else {
+            return "default";
+        }
+    }
+
+        return (
+            <Card sx={{ minWidth: 275 }}>
+                <CardContent>
+                    <Typography
+                        sx={{ fontSize: 14 }}
+                        color="text.secondary"
+                        gutterBottom
+                    >
+                        {data.dateCreated !== null
+                            ? new Date(
+                                  data.dateCreated.seconds * 1000
+                              ).toString()
+                            : "..."}
+                    </Typography>
+                    <Typography variant="h5" component="div">
+                        {data.prediction}
+                        {data.isCompleted ? " (Completed)" : " (Pending)"}
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    {data.options.map((option, index) => (
+                        <Chip
+                            key={index}
+                            variant={data.isCompleted ? "outlined" : "filled"}
+                            label={option}
+                            onClick={data.isCompleted ? null : handleClick}
+                            color={chipColor(option)}
+                            sx={{ ...padding }}
+                        />
+                    ))}
+                </CardActions>
+                <Divider />
+                <Results results={data.results} answer={data.answer} />
+            </Card>
+        );
+    };
