828b03edc99ae7150939fb661c51702944fc1af6
2vAZNTnsvzRAkB <ravenonix101@gmail.com>
2022-08-30 01:00:18 -0400

    begin addprediction form

--- client/src/components/Admin/AddPrediction.jsx
+++ client/src/components/Admin/AddPrediction.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Button, TextField } from "@mui/material";
 
 const reducer = (state, action) => {
     switch (action.type) {
@@ -16,14 +17,22 @@ const reducer = (state, action) => {
             return {
                 ...state,
                 options: state.options.filter(
-                    (option, index) => index !== action.payload
+                    (option, index) => option !== action.payload
                 ),
+            };
+        case "addOptionField":
+
+            return {
+                ...state,
+                optionField: state.optionField + 1,
             };
         case "reset":
             return {
                 prediction: "",
                 options: [],
+                optionField: 0,
             };
+
         default:
             return state;
     }
@@ -32,6 +41,43 @@ export default function AddPrediction() {
     const [state, dispatch] = React.useReducer(reducer, {
         prediction: "",
         options: [],
+        optionField: 0,
     });
-    return <div>AddPrediction</div>;
+
+    const inputs = () => {
+        let t = 1;
+        let comp = [];
+        while (t <= state.optionField) {
+            comp.push(<TextField label="Option" />);
+            t++;
+        }
+        console.log(comp);
+        return comp;
+    };
+    return (
+        <form>
+            <TextField
+                label="Prediction"
+                value={state.prediction}
+                onChange={(e) =>
+                    dispatch({
+                        type: "predictionInput",
+                        payload: e.target.value,
+                    })
+                }
+            />
+            {inputs().map((input) => input)}
+            <Button
+                variant="contained"
+                color="primary"
+                onClick={() => {
+                    dispatch({
+                        type: "addOptionField",
+                    });
+                }}
+            >
+                Add Option?
+            </Button>
+        </form>
+    );
 }
