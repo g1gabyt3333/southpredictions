6279c8e1357f30a2d8a77e88df9095f040ea084f
2vAZNTnsvzRAkB <ravenonix101@gmail.com>
2022-09-18 21:02:56 -0400

    link to profiles

--- client/src/components/LeaderboardRow.jsx
+++ client/src/components/LeaderboardRow.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { TableCell, TableRow } from "@mui/material";
+import { NavLink } from "react-router-dom";
+import { Link } from "@mui/material";
 
 export default class LeaderboardRow extends Component {
     render() {
@@ -12,8 +14,16 @@ export default class LeaderboardRow extends Component {
                 }}
             >
                 <TableCell component="th" scope="row">
-                    {this.props.data.name}
+                    <Link
+                        component={NavLink}
+                        to={`/profile/${this.props.data.id}`}
+                        color="inherit"
+                        sx={{ textDecoration: "none" }}
+                    >
+                        {this.props.data.name}
+                    </Link>
                 </TableCell>
+
                 <TableCell align="center">{this.props.data.wins}</TableCell>
                 <TableCell align="center">{this.props.data.losses}</TableCell>
             </TableRow>
